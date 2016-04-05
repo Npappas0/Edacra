f713c7e048a5b43287ab929a2535097be8f69932
NPappas <student@npappas.local>
2016-04-05 12:30:08 -0500

    Added block movement for the head of snake

--- Edacra/ViewController.swift
+++ Edacra/ViewController.swift
@@ -8,18 +8,79 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    //UI Objects
+    var block = UIView()
+    var timer = NSTimer()
+    
+    // Naming Variables
+    var xMove = CGFloat(0)
+    var yMove = CGFloat(0)
+    var xPos = CGFloat(0)
+    var yPos = CGFloat(0)
+    let directions = [UISwipeGestureRecognizerDirection.Right, UISwipeGestureRecognizerDirection.Left, UISwipeGestureRecognizerDirection.Up, UISwipeGestureRecognizerDirection.Down]
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        timerFunction()
+        block = UIView(frame: CGRectMake(150, 150
+            , 10, 10))
+        block.backgroundColor = UIColor.whiteColor()
+        self.view.addSubview(block)
+        view.backgroundColor = UIColor.blackColor()
+        
+        for direction in directions
+        {
+            let swipe = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+            swipe.direction = direction
+            self.view.addGestureRecognizer(swipe)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func timerFunction()
+    {
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "updatePosition", userInfo: nil, repeats: true)
     }
-
-
+    
+    func respondToSwipeGesture(gesture: UIGestureRecognizer)
+    {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer
+        {
+            switch swipeGesture.direction
+            {
+            case UISwipeGestureRecognizerDirection.Right:
+                xMove = 10
+                yMove = 0
+            case UISwipeGestureRecognizerDirection.Down:
+                xMove = 0
+                yMove = 10
+            case UISwipeGestureRecognizerDirection.Left:
+                xMove = -10
+                yMove = 0
+            case UISwipeGestureRecognizerDirection.Up:
+                xMove = 0
+                yMove = -10
+            default:
+                break
+            }
+        }
+    }
+    
+    @IBAction func tapStop(sender: UITapGestureRecognizer)
+    {
+        xMove = 0
+        yMove = 0
+    }
+    
+    func updatePosition()
+    {
+        xPos += xMove
+        yPos += yMove
+        self.block.transform = CGAffineTransformMakeTranslation(xPos, yPos)
+    }
 }
+
+
 
