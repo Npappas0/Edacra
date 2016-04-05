5c5c563082749623a57d49eec83737d156d2ea65
NPappas <student@npappas.local>
2016-04-05 12:08:22 -0500

    Initial Commit

---
+++ Edacra/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Edacra
+//
+//  Created by NPappas on 4/5/16.
+//  Copyright © 2016 Nick Pappas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
